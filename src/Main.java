10ffd302d6247ab5550f3a16f188f84918382ad2
duraliev <bduraliev@gmail.com>
2017-04-09 16:51:55 +0200

    small changes to move to development

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 public class Main {
 
 	/**
-	 * Concatenates a word to the phrase ", world"
+	 * Concatenates a word to the phrase ", world!"
 	 * @param word the word you want to concatenate
 	 */
 	private static void saySomethingToWorld (String word){
-		System.out.println(word + ", world!");
+		String worldString = sayWorld();
+		System.out.println(word + worldString);
+	}
+
+	/**
+	 * Returns the phrase ", world!"
+	 * @return ", world!"
+	 */
+	public static String sayWorld(){
+		return ", world!";
 	}
 
 	public static void main (String[] args){
