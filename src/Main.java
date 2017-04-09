20843635a597356a8f183a8829ec84c0891ca7fa
duraliev <bduraliev@gmail.com>
2017-04-09 17:45:20 +0200

    added addition method

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,12 @@ public class Main {
 	private static void saySomethingToWorld (String word){
 		String worldString = sayWorld();
 		System.out.println(word + worldString);
+		
+		int n1 = 2;
+		int n2 = 3;
+		int result = add(n1, n2);
+
+		System.out.println(n1 + "+" + n2 + "=" result);
 	}
 
 	/**
@@ -15,6 +21,10 @@ public class Main {
 	 */
 	public static String sayWorld(){
 		return ", world!";
+	}
+
+	public static int add(int a, int b){
+		return a + b;
 	}
 
 	public static void main (String[] args){
